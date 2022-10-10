0272b005436263b4a0cf768ef1166a232ce66cf6
amitpaultl <amitpaultl@gmail.com>
2022-10-11 00:51:06 +0600

    Footer area add

--- src/Componts/Footer/Footers.js
+++ src/Componts/Footer/Footers.js
@@ -1,13 +1,16 @@
-import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './Footers.css'
 
 const Footers = () => {
     return (
-        <div className='footer'>
-            <h2>ColorFull.com</h2>
-            <a href=""><FontAwesomeIcon icon={faCoffee}></FontAwesomeIcon></a>
+        <div className='footer p-5 my-3'>
+            <div className="container">
+
+                <h2>ColorFull.com</h2>
+                <p>Copyright post 2022</p>
+            </div>
         </div>
     );
 };
