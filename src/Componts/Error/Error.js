c2396968b73c0bbf63ed5f29f664426919d0ab48
amitpaultl <amitpaultl@gmail.com>
2022-10-11 22:37:33 +0600

    404 page conpleate

--- src/Componts/Error/Error.js
+++ src/Componts/Error/Error.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import { useRouteError } from 'react-router-dom';
+import Footers from '../Footer/Footers';
+import Header from '../Header/Header';
+
+import './Error.css'
 
 const Error = () => {
     const error = useRouteError()
     return (
         <div>
-            <h2>Error page</h2>
-            <p>
-                <i>{error.statusText || error.message}</i>
-            </p>
+            <Header></Header>
+            <div className="error-area">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="erroe-pag d-flex align-items-center">
+                                <div className="text-center w-100">
+
+                                    <h2 className='error'>404</h2>
+                                    <p className='error-name'>
+                                        <i>{error.statusText || error.message}</i>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Footers></Footers>
         </div>
     );
 };
